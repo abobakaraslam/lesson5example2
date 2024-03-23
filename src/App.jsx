35f14a03809b1336d80b487c14ac4835912b6296
abobakaraslam <m.abobakar.aslam@gmail.com>
2024-03-24 00:30:58 +0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+//File: App.jsx
+function App() {
+  return (
+    <div className="flex flex-col items-center">
+      <h1 className="text-red-700 text-center my-3 text-2xl">Author name</h1>
+
+      <p>
+        This is introduction about the author. This introduction is written in
+        paragraph element. The author is interested in programming languages
+        related to web, mobile application. The author has expertise in
+        JavaScript and Python language.
+      </p>
+    </div>
+  );
+}
+
+export default App;
